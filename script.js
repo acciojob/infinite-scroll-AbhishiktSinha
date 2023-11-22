57e743bebf83235697f7f1a5f14bef1e74d2e03f
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-22 08:55:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-//your code here!
+const container = document.getElementById("infi-list");
+let count = 1;
 
+for(let i = 0; i < 10; i++) {
+	const listElement = document.createElement("li");
+	listElement.innerText = `item ${count++}`;
+	container.appendChild(listElement);
+}
+
+container.addEventListener("scroll", ()=>{
+    if(container.scrollHeight-container.clientHeight-container.scrollTop >= 2){
+        const listElement = document.createElement("li");
+        listElement.innerText = `item ${count++}`;
+        container.append(listElement);
+        
+        const newElement = document.createElement("li");
+        newElement.innerText = `item ${count++}`;
+        newElement.innerText = `item ${count++}`;
+        container.append(newElement);
+        
+    }
+})
